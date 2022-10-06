f12bf154c4ae6c3ba1d41484a75de1f48db81f74
Wels <morettifilho7@gmail.com>
2022-10-06 19:00:33 +0200

    UPD-3
    Testing Scripts

--- PingPong/Assets/Scripts/Ball.cs
+++ PingPong/Assets/Scripts/Ball.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Vector3 direction;
+    public float speed;
 
     // Update is called once per frame
+    void Start() {
+        direction = new Vector3(UnityEngine.Random.Range(-1f,1),UnityEngine.Random.Range(-1f,1f),0);
+    }
     void Update()
     {
+        transform.position = direction+=1;
         
     }
 }
