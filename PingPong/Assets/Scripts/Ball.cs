f9c035acfdd5c372ab52835a5a51f5fe38f81bea
Wels <morettifilho7@gmail.com>
2022-10-07 20:45:37 +0200

    UPD-7
    Ball mechanic refined

--- PingPong/Assets/Scripts/Ball.cs
+++ PingPong/Assets/Scripts/Ball.cs
@@ -7,7 +7,9 @@ public class Ball : MonoBehaviour
     Rigidbody2D rb;
     public float speed;
     Vector2 direction;
-    // Start is called before the first frame update
+    private void Start() {
+        
+    }
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -18,5 +20,14 @@ public class Ball : MonoBehaviour
     void FixedUpdate()
     {
         rb.MovePosition(rb.position+direction*speed*Time.fixedDeltaTime);
+    }
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag=="Limit"){
+            direction = new Vector2(direction.x, -direction.y);
+        }
+        else{
+            direction = new Vector2(-direction.x,Random.Range(-1.0f,1.0f));
+            speed+=1;
+        }
     }
 }
